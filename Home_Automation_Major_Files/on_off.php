dcd2d7527e11ac8b940676010cae09c72798e089
Shubham Verma <vermashubham0202@gmail.com>
2020-09-13 17:54:29 +0530

    My Projects Compilation.

---
+++ Home_Automation_Major_Files/on_off.php
@@ -0,0 +1,15 @@
+<?php
+	if($_REQUEST) {
+		$num = $_REQUEST['num'];
+		if($num == 1) {
+			exec('sudo /usr/bin/python /var/www/server/onof_serv.py ' . $num);
+			echo "on";
+		}
+		elseif($num == 0) {
+			exec('sudo /usr/bin/python /var/www/server/onof_serv.py ' . $num);
+			echo "off";
+		}
+		else
+			echo "can't determine state";
+	}
+?>
